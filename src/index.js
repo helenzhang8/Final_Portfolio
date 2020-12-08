72f37a534a67a21b894b68ddb8ad0a0252fd044c
helenzhang8 <suki22889@gmail.com>
2020-12-08 18:37:39 -0500

    iiii

--- src/index.js
+++ src/index.js
@@ -135,9 +135,4 @@ export default class App extends React.Component {
   }
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"))
